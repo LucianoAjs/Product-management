f4309427c20dca19965798f40af784f269a6f45c
lucianos <lucianos@motorola.com>
2023-04-08 23:26:43 -0400

    Adjust responsive table

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CoreModule } from '@core/core.module';
+import { SharedModule } from '@shared/shared.modules';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,7 +10,13 @@ import { ProductManagerComponent } from './product-manager/product-manager.compo
 
 @NgModule({
   declarations: [AppComponent, ProductManagerComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, CommonModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    CommonModule,
+    SharedModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- frontend/src/app/product-manager/product-manager.component.ts
+++ frontend/src/app/product-manager/product-manager.component.ts
@@ -5,4 +5,45 @@ import { Component } from '@angular/core';
   templateUrl: './product-manager.component.html',
   styleUrls: ['./product-manager.component.scss'],
 })
-export class ProductManagerComponent {}
+export class ProductManagerComponent {
+  public list_items = [
+    {
+      name: 'Headset Gamer Havit',
+      description:
+        'Drivers 53mm, Microfone Plugável, P2, PC, PS4, XBOX ONE, Preto - HV-H2002D',
+      price: 'R$ 189,99',
+      purchaseDate: '06/04/2023',
+      category: 'Eletronico',
+    },
+    {
+      name: 'Processador Intel Core i5-10400F',
+      description:
+        '2.9GHz (4.3GHz Max Turbo), Cache 12MB, 6 Núcleos, 12 Threads, LGA 1200 - BX8070110400F',
+      price: 'R$ 2000',
+      purchaseDate: '06/04/2023',
+      category: 'Eletronico',
+    },
+    {
+      name: 'Teclado Mecânico Gamer HyperX Alloy MKW100',
+      description: 'Teclado Mecânico Gamer HyperX Alloy MKW100',
+      price: 'RGB, Switch Red, Full Size, Layout US - 4P5E1AA#ABA',
+      purchaseDate: '06/04/2023',
+      category: 'Eletronico',
+    },
+    {
+      name: 'Notebook Asus AMD Ryzen 5-3500U',
+      description:
+        '8GB RAM, SSD 256GB, 15,6, Radeon Vega 8, Windows 11 Home, Cinza - M515DA-BR1213W',
+      price: 'R$ 2.500,0',
+      purchaseDate: '06/04/2023',
+      category: 'Eletronico',
+    },
+    {
+      name: 'Mouse Gamer Redragon Cobra',
+      description: 'Chroma RGB, 12400DPI, 7 Botões, Preto - M711 V2',
+      price: 'R$ 109,0',
+      purchaseDate: '06/04/2023',
+      category: 'Eletronico',
+    },
+  ];
+}
